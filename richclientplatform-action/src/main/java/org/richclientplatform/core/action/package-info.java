32da818a89eb0e2748490ca19b4da3297968c806
puce <puce@users.sourceforge.net>
2012-03-11 18:30:34 +0100

    Annotation fix

--- richclientplatform-action/src/main/java/org/richclientplatform/core/action/package-info.java
+++ richclientplatform-action/src/main/java/org/richclientplatform/core/action/package-info.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 @Menus({
-    @Menu(id = "file", key = "fileMenu_displayName", mnemonic = "fileMenu_mnemonic", position = 10),
-    @Menu(id = "window", key = "windowMenu_displayName", mnemonic = "windowMenu_mnemonic", position = 800)
+    @Menu(id = "file", key = "fileMenu_displayName", mnemonicKey = "fileMenu_mnemonic", position = 10),
+    @Menu(id = "window", key = "windowMenu_displayName", mnemonicKey = "windowMenu_mnemonic", position = 800)
 })
 package org.richclientplatform.core.action;
