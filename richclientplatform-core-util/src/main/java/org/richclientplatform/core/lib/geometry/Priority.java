3c7431e3b047a70788bef9349b2f842a84895698
puce <puce@users.sourceforge.net>
2012-06-21 18:19:16 +0200

    - Priority added

---
+++ richclientplatform-core-util/src/main/java/org/richclientplatform/core/lib/geometry/Priority.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.richclientplatform.core.lib.geometry;
+
+/**
+ *
+ * @author puce
+ */
+public enum Priority {
+
+    ALWAYS,
+    NEVER,
+    SOMETIMES;
+}
