ad613dc0b02402f8ad5488cd1a30559b4098f872
puce <puce@users.sourceforge.net>
2014-06-21 02:11:41 +0200

    [#55] Provide repeating annotations - Menu - ToolBar - DockingArea

--- drombler-acp-core-action/src/main/java/org/drombler/acp/core/action/ToolBar.java
+++ drombler-acp-core-action/src/main/java/org/drombler/acp/core/action/ToolBar.java
@@ -16,6 +16,7 @@ package org.drombler.acp.core.action;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -23,6 +24,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.PACKAGE)
+@Repeatable(ToolBars.class)
 public @interface ToolBar {
 
     String id();
--- drombler-acp-core-docking/src/main/java/org/drombler/acp/core/docking/DockingArea.java
+++ drombler-acp-core-docking/src/main/java/org/drombler/acp/core/docking/DockingArea.java
@@ -16,6 +16,7 @@ package org.drombler.acp.core.docking;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -27,6 +28,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.PACKAGE)
+@Repeatable(DockingAreas.class)
 public @interface DockingArea {
 
     String id();
