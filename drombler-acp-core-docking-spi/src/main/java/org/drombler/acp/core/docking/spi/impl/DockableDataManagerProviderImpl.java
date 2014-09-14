a984fb67711a44cc2bffa7f3d39df944ca64161a
puce <puce@users.sourceforge.net>
2014-09-15 00:30:20 +0200

    [#44] Make the Docking Framework more lightweight: Remove Dockable and DockablePane from the API
    - DockableDataManagerProviderImpl added

---
+++ drombler-acp-core-docking-spi/src/main/java/org/drombler/acp/core/docking/spi/impl/DockableDataManagerProviderImpl.java
@@ -0,0 +1,40 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce).
+ * Copyright 2014 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.drombler.acp.core.docking.spi.impl;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.drombler.acp.core.docking.spi.DockableDataManagerProvider;
+import org.drombler.commons.client.docking.DockableData;
+import org.drombler.commons.client.docking.DockableDataManager;
+import org.drombler.commons.client.docking.SimpleDockableDataManager;
+
+/**
+ *
+ * @author puce
+ */
+@Component
+@Service
+public class DockableDataManagerProviderImpl<D, DATA extends DockableData> implements
+        DockableDataManagerProvider<D, DockableData> {
+
+    private final DockableDataManager<D, DockableData> dockableDataManager = new SimpleDockableDataManager<>();
+
+    @Override
+    public DockableDataManager<D, DockableData> getDockableDataManager() {
+        return dockableDataManager;
+    }
+
+}
