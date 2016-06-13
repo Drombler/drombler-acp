f75d1e979f8b0e9c5d49cf432d7ea99ff2e05f52
Florian Brunner <fbrunnerreg@gmx.ch>
2016-06-13 23:32:53 +0200

    #111 file-type-editor-registration - Javadoc added

--- drombler-acp-core-data/src/main/java/org/drombler/acp/core/data/Openable.java
+++ drombler-acp-core-data/src/main/java/org/drombler/acp/core/data/Openable.java
@@ -1,10 +1,21 @@
 package org.drombler.acp.core.data;
 
+import org.drombler.commons.context.LocalContextProvider;
+
 /**
+ * A data capability to open data handlers in editors.
  *
+ * This data capability is provided by a {@link DataCapabilityProvider}.
+ *
+ * To fully integrate with the framework register the instance provided for the data handler in it's local context.
+ *
+ * @see LocalContextProvider
  * @author puce
  */
 public interface Openable {
 
+    /**
+     * Opens the data handler in an editor.
+     */
     void open();
 }
