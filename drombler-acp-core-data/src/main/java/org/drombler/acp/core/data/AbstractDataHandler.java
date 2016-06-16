f94b61d4039cce8165e265d310ea6618269ea3f9
Florian Brunner <fbrunnerreg@gmx.ch>
2016-06-16 23:02:27 +0200

    #102 Support Single Instance Application - merge

---
+++ drombler-acp-core-data/src/main/java/org/drombler/acp/core/data/AbstractDataHandler.java
@@ -0,0 +1,55 @@
+package org.drombler.acp.core.data;
+
+import org.drombler.acp.core.commons.util.SimpleServiceTrackerCustomizer;
+import org.drombler.commons.context.Context;
+import org.drombler.commons.context.LocalContextProvider;
+import org.drombler.commons.context.SimpleContext;
+import org.drombler.commons.context.SimpleContextContent;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * An abstract data handler. It observes registered {@link DataCapabilityProvider}s and adds the found data capabilities to it's local context.
+ *
+ * You can use this class as a base class for your own data handler implementations.
+ *
+ * @see AbstractDocumentHandler
+ * @author puce
+ */
+public abstract class AbstractDataHandler implements AutoCloseable, LocalContextProvider {
+
+    private final SimpleContextContent contextContent = new SimpleContextContent();
+    private final Context localContext = new SimpleContext(contextContent);
+    private final ServiceTracker<DataCapabilityProvider, DataCapabilityProvider> dataCapabilityProviderTracker;
+
+    /**
+     * Creates a new instance of this class.
+     */
+    public AbstractDataHandler() {
+        this.dataCapabilityProviderTracker = SimpleServiceTrackerCustomizer.createServiceTracker(DataCapabilityProvider.class, this::addDataCapabilityProvider, this::removeDataCapabilityProvider);
+        dataCapabilityProviderTracker.open(true);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public Context getLocalContext() {
+        return localContext;
+    }
+
+    private void addDataCapabilityProvider(DataCapabilityProvider<?> dataCapabilityProvider) {
+        contextContent.add(dataCapabilityProvider.getDataCapability(this));
+    }
+
+    private void removeDataCapabilityProvider(DataCapabilityProvider<?> dataCapabilityProvider) {
+        contextContent.remove(dataCapabilityProvider.getDataCapability(this));
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void close() {
+        dataCapabilityProviderTracker.close();
+    }
+}
