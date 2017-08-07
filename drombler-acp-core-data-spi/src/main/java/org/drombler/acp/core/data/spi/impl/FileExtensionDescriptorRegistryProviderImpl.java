91679024169c1725b64864735223cbab4d131247
Florian Brunner <fbrunnerreg@gmx.ch>
2017-08-07 20:44:07 +0200

    #170 Provide a progress monitor - replaced deprecated annotations

--- drombler-acp-core-data-spi/src/main/java/org/drombler/acp/core/data/spi/impl/FileExtensionDescriptorRegistryProviderImpl.java
+++ drombler-acp-core-data-spi/src/main/java/org/drombler/acp/core/data/spi/impl/FileExtensionDescriptorRegistryProviderImpl.java
@@ -14,17 +14,15 @@
  */
 package org.drombler.acp.core.data.spi.impl;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Service;
 import org.drombler.acp.core.data.spi.FileExtensionDescriptorRegistryProvider;
 import org.drombler.commons.data.file.FileExtensionDescriptorRegistry;
+import org.osgi.service.component.annotations.Component;
 
 /**
  *
  * @author puce
  */
 @Component
-@Service
 public class FileExtensionDescriptorRegistryProviderImpl implements FileExtensionDescriptorRegistryProvider {
 
     private final FileExtensionDescriptorRegistry fileExtensionDescriptorRegistry = new FileExtensionDescriptorRegistry();
