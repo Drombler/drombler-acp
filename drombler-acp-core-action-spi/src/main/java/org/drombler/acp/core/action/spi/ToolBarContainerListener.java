cbed0224a4eaa2f078627bfbb659da10181a9b19
Florian Brunner <fbrunnerreg@gmx.ch>
2018-01-28 22:14:07 +0100

    #169 Provide a modular status bar - Javadoc

--- drombler-acp-core-action-spi/src/main/java/org/drombler/acp/core/action/spi/ToolBarContainerListener.java
+++ drombler-acp-core-action-spi/src/main/java/org/drombler/acp/core/action/spi/ToolBarContainerListener.java
@@ -17,15 +17,40 @@ package org.drombler.acp.core.action.spi;
 import java.util.EventListener;
 
 /**
+ * A {@link ToolBarContainer} listener.
+ *
  * TODO: more methods might be added in future
+ *
  * @param <ToolBar> the GUI-toolkit specific type of a tool bar
  * @param <ToolBarButton> the GUI-toolkit specific base type of a tool bar button.
  */
 public interface ToolBarContainerListener<ToolBar, ToolBarButton> extends EventListener {
 
+    /**
+     * The callback method when a tool bar was added.
+     *
+     * @param event the event
+     */
     void toolBarAdded(ToolBarContainerToolBarEvent<ToolBar, ToolBarButton> event);
+
+    /**
+     * The callback method when a tool bar was removed.
+     *
+     * @param event the event
+     */
     void toolBarRemoved(ToolBarContainerToolBarEvent<ToolBar, ToolBarButton> event);
 
+    /**
+     * The callback method when a tool bar button was added.
+     *
+     * @param event the event
+     */
     void toolBarButtonAdded(ToolBarContainerToolBarButtonEvent<ToolBar, ToolBarButton> event);
+
+    /**
+     * The callback method when a tool bar button was removed.
+     *
+     * @param event the event
+     */
     void toolBarButtonRemoved(ToolBarContainerToolBarButtonEvent<ToolBar, ToolBarButton> event);
 }
