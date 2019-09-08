9529188145bdfb27a26174327a7bc9783fcf5881
Florian Brunner <fbrunnerreg@gmx.ch>
2019-09-08 11:37:52 +0200

    - Javadoc - clean-up

--- drombler-acp-core-commons/src/main/java/org/drombler/acp/core/commons/util/BundleUtils.java
+++ drombler-acp-core-commons/src/main/java/org/drombler/acp/core/commons/util/BundleUtils.java
@@ -5,15 +5,30 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.wiring.BundleWiring;
 
 /**
+ * Utility class for {@link Bundle}.
  *
  * @author puce
  */
-public class BundleUtils {
+public final class BundleUtils {
 
+    /**
+     * Loads a class from a {@link Bundle}.
+     *
+     * @param bundle the bundle to load the class from
+     * @param className the fully qualified class name
+     * @return the class object
+     * @throws ClassNotFoundException
+     */
     public static Class<?> loadClass(Bundle bundle, String className) throws ClassNotFoundException {
         return bundle.loadClass(StringUtils.stripToNull(className));
     }
 
+    /**
+     * Gets the {@link ClassLoader} of the specified {@link Bundle}.
+     *
+     * @param bundle the bundle
+     * @return the class loader of the bundle
+     */
     public static ClassLoader getClassLoader(Bundle bundle) {
         return bundle.adapt(BundleWiring.class).getClassLoader();
     }
